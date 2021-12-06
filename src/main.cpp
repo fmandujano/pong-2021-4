3331a48b729ccd9a9be2635291529bf2137a5414
fmandujano <fmandujano@maquivol.com>
2021-12-06 12:00:28 -0600

    lee resx y resy de archivo INI

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,47 @@
 //========================================================================
 int main( int argc , char** argv )
 {
-	ofSetupOpenGL(640,480,OF_WINDOW);			// <-------- setup the GL context
+	int resx = 1000;
+	int resy = 1000;
 
+	//analizar (parse) el archivo config.ini
+	ifstream configfile;
+	configfile.open("data/config.ini"); //ruta relativa a donde se encuentra el ejecutable
+	
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofApp *myapp = new ofApp();
+
+	if (configfile.is_open())
+	{
+		puts("leyendo datos del archivo config.ini");
+		char * linea = new char[100];
+		char *llave, *valor;
+		while (!configfile.eof())
+		{
+			configfile.getline(linea, 100);
+			//ignorar lineas que comiencen con caracteres no aceptables
+			if (linea[0] != '/' &&
+				linea[0] != '[' &&
+				linea[0] != '\n' &&  //line feed
+				linea[0] != '\r' &&  //carriage return, solo en windows
+				linea[0] != '\0')
+			{
+				llave = strtok(linea, "=");
+				valor = strtok(NULL, "=");
+				printf("llave: %s, valor:%s\n", llave, valor);
+				if (!strcmp(llave, "resx"))
+					resx = atoi(valor);
+				if (!strcmp(llave, "resy"))
+					resy = atoi(valor);
+				//if (!strcmp(llave, "fullscreen"))
+					//fullscreen = atoi(valor) == 0 ? false : true;
+				//if (!strcmp(llave, "port"))
+					//port = atoi(valor);
+			}
+		}
+	}
 	
 	myapp->appState = ofApp::EAppState::client;
 	//leer parametros
@@ -28,7 +63,21 @@ int main( int argc , char** argv )
 		{
 			myapp->appState = ofApp::EAppState::client;
 		}
+		else if (!strcmp(argv[i], "resx"))
+		{
+			resx = atoi(argv[i + 1]);
+		}
+		else if (!strcmp(argv[i], "resy"))
+		{
+			resy = atoi(argv[i + 1]);
+		}
+
 	}
+
+
+
+
+	ofSetupOpenGL(resx, resy, OF_WINDOW);			// <-------- setup the GL context
 
 
 	ofRunApp(myapp);
